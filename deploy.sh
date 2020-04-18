4def733c5e534063ab659624cbdd6abf659e8565
lilachaimo <lilachaimo@gmail.com>
2020-04-18 21:10:13 +0300

    fix bug deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ docker push lilachyonash/multi-client:$SHA
 docker push lilachyonash/multi-server:$SHA
 docker push lilachyonash/multi-worker:$SHA
 
-ubectl apply -f k8s
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=lilachyonash/multi-server:$SHA
 kubectl set image deployments/client-deployment server=lilachyonash/multi-client:$SHA
 kubectl set image deployments/worker-deployment server=lilachyonash/multi-worker:$SHA
